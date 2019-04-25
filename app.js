0a8f711fbea75f6914f05f15b3d9cfa4255f6e97
Mirana09 <mirana.rakotomavo09@gmail.com>
2019-04-26 00:23:56 +0200

    port2

--- app.js
+++ app.js
@@ -14,8 +14,8 @@ app.use(express.static(__dirname + '/public'));
 
 datalayer.init(function(){
     console.log('init');
-    app.listen(5000);
-    console.log("Listening on port 5000");
+    app.listen(process.env.PORT || 3000);
+    console.log("Listening on port 3000");
 });
 
 app.get('/', function(req,res){
